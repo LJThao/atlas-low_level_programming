9c10aa711dfcfe206f5c8060c80ab95b958c61a4
LJ <yengfong.thao@atlasschool.com>
2024-01-22 01:20:58 -0800

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void ()
+ */
+
+void print_alphabet(void)
+{
+char  i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
