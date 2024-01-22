c087a56f63c34060372241c211a825c1fc3177e0
LJ <yengfong.thao@atlasschool.com>
2024-01-21 22:26:22 -0800

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-/**
- * main - A program that print the size of the various tyes on the computer;
- * Return: 0 (Success);
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
-}
 
+/**
+ *
+ *  * main - Write a C program that prints exactly using the printf
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
