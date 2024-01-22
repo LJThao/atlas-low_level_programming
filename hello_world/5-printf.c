5c333c640f45f0187c0cf9722fede982efb933f0
LJ <yengfong.thao@atlasschool.com>
2024-01-21 22:08:36 -0800

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - A program that pris the size of the various tyes on the computer
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
+		
