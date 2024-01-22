6086e84fa4b98360d12b9ea1008cdfa7b1a7de59
LJ <yengfong.thao@atlasschool.com>
2024-01-21 23:00:40 -0800

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ *
+ * main - Smile in the mirror
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		char low;
+
+
+
+			for (low = 'z'; low >= 'a'; low--)
+
+						putchar(low);
+
+					putchar('\n');
+
+
+
+						return (0);
+
+}
