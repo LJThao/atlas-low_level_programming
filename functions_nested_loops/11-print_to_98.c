189fffbd929c25a2f418af454627188c08d329ce
LJ <yengfong.thao@atlasschool.com>
2024-01-22 02:58:10 -0800

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,10 +13,12 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n == 98)
+		for (; n <== 98; n++)
 		{
+			if (n == 98)
+			{
 			printf("%d", n);
-			printf("n");
+			printf("\n");
 			break;
 		}
 		else
