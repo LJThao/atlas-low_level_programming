e84060311262e2129721bf2560a908500797a3fa
LJ <yengfong.thao@atlasschool.com>
2024-01-21 22:15:56 -0800

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that pris the size of the various tyes on the computer
+ * main - A program that print the size of the various tyes on the computer
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art.\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art.\n")
+return (0);
 }
 		
