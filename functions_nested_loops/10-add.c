3b007d94761a10e3800e1c7f93a508e2db4c52a8
LJ <yengfong.thao@atlasschool.com>
2024-01-22 02:25:14 -0800

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @c1: The first number to be computed
+ * @c2: The second number to be computed
+ * Return: Integer value
+ */
+
+int add(int c1, int c2)
+
+{
+
+		return (c1 + c2);
+
+}
