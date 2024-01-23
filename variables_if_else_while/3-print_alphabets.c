b087690a79b20d515d98b8ec7905f2c69dc96971
LJ <yengfong.thao@atlasschool.com>
2024-01-22 18:50:26 -0800

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,14 +11,14 @@
 int main(void)
 
 {
-	char low, e, q;
-	e = 'e';
-	q = 'q';
-	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
-	putchar('\n');
-	return (0);
+	char low;
+	
+	for (low = 'a' ; low <= 'z' ; low++)
+		putchar(low);
+
+	for (low = 'A' ; low <= 'Z' ; low++)
+		putchar(low);
+		putchar('\n');
+
+		return (0);
 }
