990275c40782005687f76931ddfb9445a9c54e42
LJ <yengfong.thao@atlasschool.com>
2024-01-23 14:15:42 -0800

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Write a C program that prints the size of various types on the computer it is compiled and run on.
+ * main - prints the size of various types
  * Return: Always 0 (Success)
  */
 
