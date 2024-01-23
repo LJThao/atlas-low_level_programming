52809f50d25ce9ed5da7ccf632b804b493e6bf30
LJ <yengfong.thao@atlasschool.com>
2024-01-22 20:03:36 -0800

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-/**
- *
- *  * main - Prints alphabet
- *
- *   * letter: letter
- *
- *    *
- *
- *     * Description: Prints alphabet ascendently
- *
- *      * Return: Always 0
- *
- *       */
+
+/** main - Prints the alphabet in lowercase
+ * Return: Always 0
+ */
+
 int main(void)
+
 {
-		char letter;
-		for (letter = 'a'; letter <= 'z'; letter++)
-{
-		putchar(letter);
-		if (letter == 'z')
-{
-		putchar('\n');
-}
-}
-return (0);
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	putchar(letter);
+	if (letter == 'z')
+		
+	putchar('\n');
+
+	return (0);
 }
