4f132d6b5772248946fa7574a3d090871e577c6b
LJ <yengfong.thao@atlasschool.com>
2024-01-22 18:18:56 -0800

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point - Positive anything is better than negative nothing
- * Return: Always 0 (Success)
+ * main - Positive anything is better than negative nothing
+ * Return: Always (Success)
  *
  */
 
@@ -18,16 +18,15 @@ int main(void)
 
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
-	}	
+		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
-	{
-        printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
-
 }
