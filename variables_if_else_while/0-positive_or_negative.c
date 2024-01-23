c6502f0403835f33923d765aa0b3e0d4cc84e42d
LJ <yengfong.thao@atlasschool.com>
2024-01-22 17:38:37 -0800

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  *
  * main - Positive anything is better than negative nothing
  * void - empty
  * Return: Always 0 (Success)
- */
+
+*/
 int main(void)
 {
 	int n;
+
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
