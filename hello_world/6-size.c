7814fd51e09ba7ce535b90caece1a641f3783ce7
LJ <yengfong.thao@atlasschool.com>
2024-01-22 21:10:28 -0800

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
  * main - Write a C program that prints the size of various types on the computer it is compiled and run on.
- *
  * Return: Always 0 (Success)
- *
  */
-int main(void)
-{
 
+int main(void)
+
+{	
 	printf("Size of a char: %1d byte(s)\n", sizeof(char));
 	printf("Size of an int: %1d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
