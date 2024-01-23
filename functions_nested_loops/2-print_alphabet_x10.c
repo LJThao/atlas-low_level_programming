dec36200f88cb960597dc1fc6bf60ff89a159ef6
LJ <yengfong.thao@atlasschool.com>
2024-01-22 20:57:59 -0800

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,16 @@
 void print_alphabet_x10(void)
 
 {
-  char c;
-  int i = 0;
-  while (i <= 9)
-   {
-    for (c = 'a'; c <= 'z'; c++)
+	char c;
+	int i = 0;
+	
+	while (i <= 9)
 	{
-	  _putchar(c);
-  	}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}	
 	_putchar('\n');
 	i++;
-       }
+	}
 }
