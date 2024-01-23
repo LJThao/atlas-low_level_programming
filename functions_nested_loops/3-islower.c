47db257744ea4a747de0083e698dfee1a6167bfa
LJ <yengfong.thao@atlasschool.com>
2024-01-23 14:31:52 -0800

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,17 +8,14 @@
  * Return: 1 for lowercase char or 0 for anything else
  *
  */
+
 int _islower(int c)
+
 {
-
-	  if (c >= 97 && c <= 122)
-
-		      {
-
-			            return (1);
-
-				        }
-
-	    return (0);
+	if (c >= 97 && c <= 122)
+	{
+		return (1)
+	}
+	return (0);
 
 }
