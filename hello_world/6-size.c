94a16087667026176d98c54897ef21a1e5778763
LJ <yengfong.thao@atlasschool.com>
2024-01-22 21:17:02 -0800

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,6 +6,7 @@
  */
 
 int main(void)
+
 {
 	printf("Size of a char: %1d byte(s)\n", sizeof(char));
 	printf("Size of an int: %1d byte(s)\n", sizeof(int));
@@ -13,4 +14,5 @@ int main(void)
 	printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %1d byte(s)\n", sizeof(float));
 	return (0);
+
 }
