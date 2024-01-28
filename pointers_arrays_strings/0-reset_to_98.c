80b61f2a0f275c03193245e85d73779986bc2605
LJ <yengfong.thao@atlasschool.com>
2024-01-27 16:00:40 -0800

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a function that takes a pointer to an int i as a parameter
+ * reset_to_98 - a function that takes a pointer to an int
  *
  * Return: Always 0
  */
 
-int reset_to_98(int *i)
+int reset_to_98(int *n)
 
 {
-	*i = 98;
+	*n = 98;
 
 }
