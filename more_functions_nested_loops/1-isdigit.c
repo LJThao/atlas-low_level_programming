72cf8cdf64f3bc2255859781306fa9b6bddfc689
LJ <yengfong.thao@atlasschool.com>
2024-01-28 18:46:52 -0800

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a digit 0 through 9
+ * @c: c is the digit
+ * Return: Always 0
+ *
+ */
+
+int _isdigit(void)
+{
+	char c;
+
+	c = '0';
+
+	c = 'a';
+
+	return (0);
+}
