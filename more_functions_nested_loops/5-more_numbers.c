2df98cce0e7ed57aa6326bcfa38321e3d2719101
LJ <yengfong.thao@atlasschool.com>
2024-01-28 21:42:23 -0800

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
+
 /**
  * more_numbers - prints 10 times 0 to 14
- * Return: 10 multiply by the numbers of 0 to 14
+ * Return: 10 times by the numbers of 0 to 14
  */
 
 void more_numbers(void)
 {
 	int l, j;
+
 	for (l = 0; l < 10; l++)
+	{
 	for (j = 0; j <= 14; j++)
+	{
 	if (j > 9)
 	{
 	_putchar((j / 10) + '0');
