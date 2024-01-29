6314762eeb84190728b9eaba6600851535d74dc6
LJ <yengfong.thao@atlasschool.com>
2024-01-28 20:28:13 -0800

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * @n: the numbers
+ * Return: Always (0)
+ */
+
+void print_numbers(void)
+{
+	int n;
+	
+	for (n = 0; n <= 9; x++)
+	{
+	_putchar(n + '0');
+	}
+	_putchar('\n');
+}
