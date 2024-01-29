3b5f35a680d13faa0f748fea4b70578992862902
LJ <yengfong.thao@atlasschool.com>
2024-01-28 21:00:18 -0800

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,13 @@
 /**
  * print_numbers - prints the numbers from 0 to 9
  * @a: the numbers
- * Return: Always (0)
+ * Return: numbers since 0 up to 9
  */
 
 void print_numbers(void)
 {
 	int a;
+
 	for (a = 0; a <= 9; a++)
 	{
 	_putchar(a + '0');
