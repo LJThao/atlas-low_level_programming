334ff2f025d88e98808b16cf17de7a95c3e2407b
LJ <yengfong.thao@atlasschool.com>
2024-01-28 23:15:11 -0800

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * _strlen - returns the length of a string
  * @str: string of length
- * Return: length of @str
+ * Return: length of string
  */
 
 size_t _strlen(const char *str)
