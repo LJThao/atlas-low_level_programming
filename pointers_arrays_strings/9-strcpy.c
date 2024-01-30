cb28e910148f9bc37470570e47f8b2a1d26969be
LJ <yengfong.thao@atlasschool.com>
2024-01-29 20:09:11 -0800

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _strcpy - copies the string
  * @dest: dest value
@@ -13,6 +12,7 @@ char *_strcpy(char *dest, char *src)
 	int t;
 	for (t = 0; src[t] != '\0'; t++)
 	{
+
 	dest[t] = src[t];
 	}
 	dest[t++] = '\0';
