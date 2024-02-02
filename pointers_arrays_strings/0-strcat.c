0c409f9ed1fef01359f5e8ce32144014ca9ac063
LJ <yengfong.thao@atlasschool.com>
2024-02-01 19:56:18 -0800

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - Write a function that concatenates two strings
+ * @src: src string
+ * @dest: dest string
+ * Return: the dest string
+
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+	while (dest[dlen])
+	{
+	dlen++;
+	}
+	for (i = 0; src[i] != 0; i++;
+	{
+	dest[dlen] = src[i];
+	dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
