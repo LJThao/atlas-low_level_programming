9534e012d8a830b90dd404557e435def662c6be2
LJ <yengfong.thao@atlasschool.com>
2024-02-05 15:09:06 -0800

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * @m: string
+ * Return: The m string
+ */
+
+char *leet(char *m)
+{
+	int l = 0, j = 0, 1 = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (m[l])
+	{
+		j = 0;
+		
+		while (j < 1)
+		{
+			if (m[l] == r[j] || m[l] - 32 == r[j])
+			{
+				m[l] = n[j];
+			}
+			j++;
+		}
+		l++;
+	}
+	return (m);
+}
