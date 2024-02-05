b3cd50e83fd9a85cddc1b9813c3c41af85554c4c
LJ <yengfong.thao@atlasschool.com>
2024-02-05 15:21:56 -0800

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area s
+ * @b: constand byte b
+ * @n: bytes of the memory area
+ * Return: Always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int l;
+
+	for (l = 0; l <n; l++)
+		s[l] = b;
+	return (s);
+}
