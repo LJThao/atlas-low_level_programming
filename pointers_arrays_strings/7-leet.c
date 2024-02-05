99f4e0ab0e129b859a849647f6d0535b4905e13f
LJ <yengfong.thao@atlasschool.com>
2024-02-05 15:12:47 -0800

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 
 char *leet(char *m)
 {
-	int l = 0, j = 0, 1 = 5;
+	int l = 0, j = 0, t = 5;
 	char r[5] = {'A', 'E', 'O', 'T', 'L'};
 	char n[5] = {'4', '3', '0', '7', '1'};
 
@@ -16,7 +16,7 @@ char *leet(char *m)
 	{
 		j = 0;
 		
-		while (j < 1)
+		while (j < t)
 		{
 			if (m[l] == r[j] || m[l] - 32 == r[j])
 			{
