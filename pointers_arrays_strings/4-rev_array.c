08ccc3b6a6a6a0d435e082303b56256026f114c6
LJ <yengfong.thao@atlasschool.com>
2024-02-05 14:46:04 -0800

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_array - Write a function that reverses the content of an array of integers.
+ * reverse_array - Reverses the content of an array of integers.
  * @a: array of integers
  * @n: number of elements
  */
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n)
 {
 	int lj, t;
 
-	for (t = n - 1; t >=n / 2; t--)
+	for (t = n - 1; t >= n / 2; t--)
 	{
 	lj = a[n - 1 - t];
 	a[n - 1 - t] = a[t];
