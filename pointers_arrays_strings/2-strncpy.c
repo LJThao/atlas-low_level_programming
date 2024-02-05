ab26d0ce258d0a9515d446d5b27323dbcb8f0906
LJ <yengfong.thao@atlasschool.com>
2024-02-05 14:16:17 -0800

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** _strncpy - Write a function that copies a string.
+ * @dest: dest string.
+ * @src: source string.
+ * @n: int n of bytes.
+ * Return: string to dest string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int lj = 0, src_len = 0;
+
+	while (src[lj++])
+	src_len++;
+	
+	for (lj = 0; src[lj] && lj < n; lj++)
+	dest[lj] = src[lj];
+
+	for (lj = src_len; lj < n; lj++)
+	dest[lj] = '\0';
+
+	return (dest);
+}
