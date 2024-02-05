c7cdb3da164d172522197f3303a191b6a88daece
LJ <yengfong.thao@atlasschool.com>
2024-02-05 14:32:27 -0800

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strcmp - Write a function that compares two strings.
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: h value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int l = 0, j = 0, t = 0, h = 0, lim;
+
+	while (s1[l])
+	{
+		l++;
+	}
+
+	while (s2[j])
+	{
+		j++;
+	}
+
+	if (l <= j)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+
+	while (t <= lim)
+	{
+		if (s1[t] == s2[t])
+		{
+			t++;
+			continue;
+		}
+		else
+		{
+			h = s1[t] - s2[t];
+			break;
+		}
+
+		t++;
+	}
+	return (h);
+}
