0cf4ddb700e320ac865335649258bedf6bc57910
LJ <yengfong.thao@atlasschool.com>
2024-02-12 15:07:45 -0800

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: int n to find the square root
+ * Return: square root of the number
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_rt(n, 1));
+}
+
+/**
+ * _rt - finding square root
+ * @n: int n
+ * @i: int i
+ * Return: square root of the number
+ */
+
+int _rt(int n, int i)
+{
+	int rt = i * i;
+
+	if (rt > n)
+		return (-1);
+
+	if (rt == n)
+		return (i);
+	return (_rt(n, i + 1);
+}
