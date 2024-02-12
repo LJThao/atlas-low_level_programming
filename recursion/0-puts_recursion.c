760fdc721933afaeb3194145222458ba2a9c2ebe
LJ <yengfong.thao@atlasschool.com>
2024-02-12 13:43:56 -0800

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,17 +3,18 @@
 /**
  * _puts_recursion - a function that prints a string
  * @s: s string
- * Return: Always 0
+ * Return: Nothing
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
-
-	else
-		_putchar('\n')
+	
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
--- recursion/_putchar.c
+++ recursion/_putchar.c
@@ -4,12 +4,9 @@
 /**
  * _putchar - my putchar.c
  * @c: c character
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- *
+ */
 
 int _putchar(char c)
 {
-	return (write(1, &x, 1));
+	return write(STDOUT_FILENO, &c, 1);
 }
