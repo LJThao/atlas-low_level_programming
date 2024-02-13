54b7ddee5e6d11bb83f11efe82e0b10dcad31778
LJ <yengfong.thao@atlasschool.com>
2024-02-12 17:28:26 -0800

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the # of arguments
+ * @argc: argument c
+ * @argv: argument v
+ * Return: Always
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0];
+	
+	return (0);
+}
