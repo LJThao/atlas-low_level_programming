091bc4550dc50e2bdf7061c6576a322294157cd9
LJ <yengfong.thao@atlasschool.com>
2024-02-12 16:41:23 -0800

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * whatsmyname - print its name
+ * @lj: argument c
+ * @th: argument v
+ * Return: Always (0)
+ */
+
+int whatsmyname(int lj, char *th[])
+{
+	(void) th;
+	printf("%s\n", th[0]);
+
+	return(0);
+}
