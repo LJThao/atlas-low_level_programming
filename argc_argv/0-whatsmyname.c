b24d44ea3cfac675fa746162fa1c47f0233776e9
LJ <yengfong.thao@atlasschool.com>
2024-02-12 16:46:52 -0800

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,13 +5,13 @@
  * main - print its name
  * @argc: argument c
  * @argv: argument v
- * Return: Always (0)
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
+	(void) argc;
 	printf("%s\n", argv[0]);
-}
+
 	return 0;
 }
