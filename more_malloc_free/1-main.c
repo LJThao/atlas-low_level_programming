ec8c12ab0ade5e26f1fae83c8d4ebd56250c8fe9
LJ <yengfong.thao@atlasschool.com>
2024-02-19 15:23:48 -0800

    1-string_nconcat.c

--- more_malloc_free/1-main.c
+++ more_malloc_free/1-main.c
@@ -1,35 +1,19 @@
 #include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
- *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * main - check the code
+ * Return: Always 0.
+ */
 
 int main(void)
-
 {
+	char *concat;
 
-	    char *concat;
+	concat = string_nconcat("Best ", "School !!!", 6);
+    	printf("%s\n", concat);
+        free(concat);
 
-
-
-	        concat = string_nconcat("Best ", "School !!!", 6);
-
-		    printf("%s\n", concat);
-
-		        free(concat);
-
-			    return (0);
-
+    	return (0);
 }
