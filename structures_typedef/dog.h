f4f2e9e8844a9813a93d59b544207ca412256bff
LJ <yengfong.thao@atlasschool.com>
2024-02-26 23:08:46 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog - Poppy
+ * @name - name
+ * @age - age
+ * @owner - owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
