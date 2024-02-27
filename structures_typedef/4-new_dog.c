3e2edaf312d8f8e73f4e851331f32f2aa5b8aa5e
LJ <yengfong.thao@atlasschool.com>
2024-02-27 05:41:18 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,76 +1,52 @@
 #include "dog.h"
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> 
 
 /**
  * new_dog - function that creates a new dog
  * @name: name
  * @age: age
  * @owner: owner
- * Return: 0
+ * Return: ptr to dog
  */
 
-dog_t *new_dog(char *name, float age, char *owner) {
-	dog_t *lj-dog;
-	int name-1 = 0, owner-1 = 0;
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int new;
+	char *ptr
+	dog_t *d;
 
-	if (name != NULL && owner !=NULL) {
-		name-1 = _strlen(name) + 1;
-		own-1 = strlen(owner) + 1;
-		lj-dog = malloc(sizeof(dog_t));
+	if (name == 0 || owner == 0)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 
-		if (lj-dog == NULL)
-			return NULL;
-
-		lj-dog->name = malloc(sizeof(char) * name-1);
-
-		if (lj-dog->name == NULL) {
-			free(lj-dog);
-			return NULL;
-		}
-
-		lj-dog->owner = malloc(sizeof(char) * owner-1);
-
-		if (lj-dog->owner == NULL) {
-			free(lj-dog->name);
-			free(lj-dog);
-			return NULL;
-		}
-
-		lj-dog->name = _strcpy(lj-dog->name, name);
-		lj-dog->owner = _strcpy(lj-dog->owner, owner);
-		lj-dog->age = age;
+	for (new = 1, ptr = name; *ptr; new++)
+		ptr++;
+	d->name = malloc(new);
+	if (d->name == 0)
+	{
+		free(d);
+		return (NULL);
 	}
-	return (lj-dog);
-}
 
-/**
- * _strlen - returning the length of the string
- * @s: string
- * Return: string
- */
-
-int _strlen(char *s) {
-	int t = 0;
-
-	for (; *s != '\0'; s++) {
-		t++;
+	for (new = 1, ptr = owner; *ptr; new++)
+		ptr++;
+	d->owner = malloc(new);
+	if (d->owner == 0)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
 	}
-	return (t);
-}
 
-/**
- * _strcpy - a copied string
- * @dest: dest
- * @src: src
- * Return: all
- */
+	for (new = 0; *name != 0; new++, name++)
+		d->name[new] = *name;
+	d->name[new] = 0;
+	for (new = 0; *owner != 0; new++)
+		d->owner[new] = *owner++;
+	d->owner[new] = 0;
+	d->age = age;
 
-char *_strcpy(char *dest, char *src) {
-	int a;
-
-	for (a = 0; src[a] != '\0'; a++) {
-		dest[a] = src[a];
-	return (dest);
-	}
+	return (d);
 }
