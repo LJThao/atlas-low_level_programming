5a0736614f4eb05bbcfed2684ded3b67aed92461
LJ <yengfong.thao@atlasschool.com>
2024-02-27 03:00:56 +0000

    0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum
+ * @n: int n
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int s = 0;
+	int sum = 0;
+
+	if (n != 0)
+	{
+		va_start(args, n);
+
+		while(s < n)
+		{
+			sum += va_arg(args, int);
+			s++;
+		}
+		va_end(args);
+		return (sum);
+	}
+	return (0);
+}
