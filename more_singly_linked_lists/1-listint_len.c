d08444390edde81f703eba6fa16f6abc2c31db62
LJThao <yengfong.thao@atlasschool.com>
2024-03-11 23:09:19 +0000

    1-listint_len.c

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -8,12 +8,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-        size_t node = 0;
-
-        while (h)
-        {
-                node++;
-                h = h->next;
-        }
-        return (node);
+	size_t node = 0;
+	
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
 }
