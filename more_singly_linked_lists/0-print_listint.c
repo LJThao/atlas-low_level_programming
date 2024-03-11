283714d604541ef832b81db511dea6f7f8dd38be
LJThao <yengfong.thao@atlasschool.com>
2024-03-11 23:01:34 +0000

    0-print_listint.c

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -8,20 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t node = 1;
+	size_t node = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-
-		h = h->next;
 		node++;
 
+		h = h->next;
 	}
-	printf("%d\n", h->);;
-
 	return (node);
 }
