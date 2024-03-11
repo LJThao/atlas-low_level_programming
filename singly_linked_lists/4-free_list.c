4ea53ef9d7f1f4c6b27605c315390c3c7cfab4bf
LJThao <yengfong.thao@atlasschool.com>
2024-03-11 22:17:47 +0000

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * free_list - function that frees a list_t
+  * @head: head list
+  * Return: 0
+  */
+
+void free_list(list_t *head)
+{
+	list_t list;
+
+	while (head)
+	{
+		list = head;
+		head = head->next;
+		free(list->str);
+		free(list;
+	}
+	free(head);
+}
