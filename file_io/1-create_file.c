41f19be8aedf71c168b94a20cc667756569a7b50
LJThao <yengfong.thao@atlasschool.com>
2024-03-12 02:34:56 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 			z++;
 		y = write(file_x, text_content, z);
 	}
-	if (close(file) == -1 || z != y)
+	if (close(file_x) == -1 || z != y)
 		return (-1);
 	return (1);
 }
