f9de5602e3e498b14254728a49d5feae1addd13c
LJThao <yengfong.thao@atlasschool.com>
2024-03-12 01:35:19 +0000

    1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,21 +9,21 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int bin = 1ul << 63;
-	char c = '0';
+	char a = '0';
 
 	while (!(bin & n) && bin != 0)
 		bin = bin >> 1;
 
 	if (bin == 0)
-		write(1, &c, 1);
+		write(1, &a, 1);
 
 	while (bin)
 	{
 		if (bin & n)
-			c = '1';
+			a = '1';
 		else
-			c = '0';
-		write(1, &c, 1);
+			a = '0';
+		write(1, &a, 1);
 		bin = bin >> 1;
 	}
 }
