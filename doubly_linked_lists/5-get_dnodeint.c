fcd5d3342d0b6265a6a7413f2e83742714fd774b
LJThao <yengfong.thao@atlasschool.com>
2024-03-18 18:37:17 +0000

    5-get_dnodeint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function tht returns the nth node
+ * @head: head of list
+ * @index: index of node
+ * Return: nth node or return NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int a;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head->prev != NULL)
+		head = head->prev;
+
+	a = 0;
+
+	while (head != NULL)
+	{
+		if (a == index)
+			break;
+		head = head->next;
+		a++;
+	}
+	return (a);
+}
