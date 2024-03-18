549d3fe28f4848a2da63d9616a5157e142ad9834
LJThao <yengfong.thao@atlasschool.com>
2024-03-18 05:48:16 +0000

    0-print_dlistint.c

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elements of a list
+ * @h - head of the list
+ * Return: nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int add;
+
+	add = 0;
+
+	if (h == NULL)
+		return (add);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		add++;
+		h = h->next;
+	}
+	return (add);
+}
