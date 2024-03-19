194d5aef7b8d80ffc5f332c1ad6a23fd46305ec9
LJThao <yengfong.thao@atlasschool.com>
2024-03-19 01:31:08 +0000

    change

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adding the new node
+ * @head: head of list
+ * @n: new node
+ * Return: address of new
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t a;
+	dlistint_t b;
+	
+	a = malloc(size of(dlistint_t));
+	if (a == NULL)
+		return (NULL);
+
+	a->n = n;
+	a->prev = NULL;
+	b = *head;
+
+	if (b != NULL)
+	{
+		while (b->prev != NULL)
+			b = b->prev;
+	}
+	a->next = b;
+
+	if (b != NULL)
+		b->prev = a;
+
+	*head = a;
+
+	return (a);
+}
